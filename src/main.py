741e20f70ef97d83c1b07f13a19c5c7e47e8cc75
t_kubo <t_kubo.tome@outlook.jp>
2026-02-06 09:42:31 +0900

    Update handling of tool_id

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import multiprocessing
 
 
@@ -5,27 +6,23 @@ if __name__ == '__main__':
     # Freeze Support for Windows
     multiprocessing.freeze_support()
 
+    parser = argparse.ArgumentParser()
 
     # NOTE: 現在ロボットに付いているツールが何かを管理する方法がないので
     # ロボット制御コードの使用者に指定してもらう
-    # ツールによっては、ツールとの通信が不要なものがあるので、通信の成否では判定できない
+    # ツールによっては、ツールとの通信が不要なものがあるので、通信の成否では
+    # 現在ロボットに付いているツールが何かを判定できない
     # 現在のツールの状態を常にファイルに保存しておき、ロボット制御コードを再起動するときに
     # そのファイルを読み込むようにすれば管理はできるが、エラーで終了したときに
-    # ファイルの情報が正確かいまのところ保証できないので、指定してもらう
-    import argparse
-    parser = argparse.ArgumentParser()
-    # NOTE: Doosanでは現状1つのツールに対応
-    # parser.add_argument(
-    #     "--tool-id",
-    #     type=int,
-    #     required=True,
-    #     choices=tool_ids,
-    #     help="現在ロボットに付いているツールのID",
-    # )
+    # ファイルの情報が正確かいまのところ保証できないので、指定してもらうことにしている
+    # 管理する方法が確立されたら変更される可能性あり
+    from .doosan.doosan_tools import tool_infos
     parser.add_argument(
-        "--use-joint-monitor-plot",
-        action="store_true",
-        help="関節角度のモニタープロットを使用する",
+        "--tool-id",
+        type=int,
+        required=len(tool_infos) > 1,
+        choices=[tool_info["id"] for tool_info in tool_infos],
+        help="現在ロボットに付いているツールのID",
     )
     parser.add_argument(
         "--gui",
@@ -39,12 +36,13 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
     kwargs = vars(args)
+
+    # HACK: 現在ロボットに付いているツールが何かを管理する方法が確立されたら
+    # コードが変更される可能性があるため、変更しやすいように引数で渡すのではなく
+    # 環境変数でグローバル変数として使う
     import os
-    # HACK: コードの変化を少なくするため、
-    # ロボット制御プロセスに引数で渡すのではなく環境変数で渡す
-    # os.environ["TOOL_ID"] = str(kwargs.pop("tool_id"))
-    # NOTE: 現状URではツールIDは1固定
-    os.environ["TOOL_ID"] = "1"
+    os.environ["TOOL_ID"] = str(kwargs.pop("tool_id"))
+
     if kwargs.pop("gui"):   
         import tkinter as tk
         from doosan.gui import MQTTWin
