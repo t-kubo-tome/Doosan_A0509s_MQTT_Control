4c3bfd9f5bd87daeaa33be5ac71cac173f22bb10
t_kubo <t_kubo.tome@outlook.jp>
2026-03-18 18:03:01 +0900

    Fix import

--- src/common/control_save.py
+++ src/common/control_save.py
@@ -6,7 +6,7 @@ import time
 from typing import TextIO
 
 from ..robot.config import T_INTV, save_control
-from .shared_memory import NamedSharedMemory
+from ..robot.shared_memory import NamedSharedMemory
 
 
 class CON_Archiver:
